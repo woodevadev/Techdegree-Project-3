1f3b339c854c76d29394418a814e7692806c5d2c
Alan Wood <woodevadev@gmail.com>
2018-05-15 13:27:01 -0400

    Added total for activities functionality

--- js/app.js
+++ js/app.js
@@ -75,12 +75,12 @@ function disableEnable(index, eOrD){
     }
 }
 
-//Appends and h5 tag to the bottom of the 
+//Appends an h4 tag to the bottom of the 
 //activities fieldset and hides it
-const anh5 = $('<h5></h5>');
-$('.activities').append(anh5);
-let $totalh5 = $('.activites h5');
-$totalh5.hide();
+const anh4 = $('<h4 class="total">Total:</h4>');
+$('.activities').append(anh4);
+let $totalh4 = $('.total');
+$($totalh4).hide();
 
 //This function checks to see if any of the
 //activities are checked.  If not then the total
@@ -101,7 +101,7 @@ let total = 0;
 
 $('.activities').on('click', 'input', function(event){
     //This block handles the disabling of same time
-    //events
+    //events if there is a conflict
     let boxIndex = ($(event.target).parent().index());
     if(boxIndex == 2){;
         if($($inputSelector[1]).is(":checked") == true){
@@ -113,40 +113,77 @@ $('.activities').on('click', 'input', function(event){
     if(boxIndex == 4){
         if($($inputSelector[3]).is(":checked") == true){
             disableEnable(1, 'disable');
-        }else if($($inputSelector[1]).is(":checked") == false){
+        }else if($($inputSelector[3]).is(":checked") == false){
             disableEnable(1, 'enable');
         } 
     }
     if(boxIndex == 3){;
         if($($inputSelector[2]).is(":checked") == true){
             disableEnable(4, 'disable');
-        }else if($($inputSelector[1]).is(":checked") == false){
+        }else if($($inputSelector[2]).is(":checked") == false){
             disableEnable(4, 'enable');
         }
     } 
     if(boxIndex == 5){
         if($($inputSelector[4]).is(":checked") == true){
             disableEnable(2, 'disable');
-        }else if($($inputSelector[1]).is(":checked") == false){
+        }else if($($inputSelector[2]).is(":checked") == false){
             disableEnable(2, 'enable');
         } 
     }
-    //This block
-    if(anyChecked()){
-        $totalh5.show();
-        $($inputSelector).each(function(index){
-            if($($inputSelector[index]).is(':checked')){
-                if(index == 1){
-                    total = total + 200;
-                }else{
-                    total = total + 100
-                }
+
+    //This block controls the total amount shown
+    //if the amount is zero then the total identifier
+    //is hidden
+    if(anyChecked){
+        $($totalh4).show();
+        if(boxIndex == 1){
+            if($($inputSelector[boxIndex - 1]).is(':checked') == true){
+                total = total + 200;
+            }else{
+                total = total - 200;
             }
-        });
-        $($totalh5).text(`<h5>Total: ${total}</h5>`);
+        }else if(boxIndex > 1){
+            if($($inputSelector[boxIndex - 1]).is(':checked') == true){
+                total = total + 100;
+            }else{
+                total = total - 100;
+            }
+        }
+        if(total == 0){
+            $($totalh4).hide();    
+        } 
+        $($totalh4).html(`<h4>Total: $${total}</h4>`);
     }else{
+        $($totalh4).hide();
         total = 0;
-        $totalh5.hide();
-    }
+    }   
 });
 
+//Selectors for the different payment methods
+//information
+const $divCreditCard = $('#credit-card');
+const $divPaypal = $($divCreditCard).next();
+const $divBitcoin = $($divPaypal).next();
+
+//Hide each element to be shown when a 
+//selection is made
+$divCreditCard.hide();
+$divPaypal.hide();
+$divBitcoin.hide();
+
+//Handles the showing of different payment
+//informations
+$('#payment').on('click',function(event){
+    if($(event.target).val() == 'select_method'){
+        $divCreditCard.hide();
+        $divPaypal.hide();
+        $divBitcoin.hide();
+    }else if($(event.target).val() == 'credit card'){
+        $divCreditCard.show();
+    }else if($(event.target).val() == 'paypal'){
+        $divPaypal.show();
+    }else if($(event.target).val() == 'bitcoin'){
+        $divBitcoin.show();
+    }
+});
